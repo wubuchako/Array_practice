ee311778e8b266d62a80b081e8d381ce1a4c6c93
wubuchako <56101474+wubuchako@users.noreply.github.com>
2019-11-24 11:00:24 +0900

    array practice

--- array.js
+++ array.js
@@ -1,7 +1,18 @@
+// 情報を蓄える
 var todos = ["item 1", "item 2", "item 3"]
 todos
 
+// 情報を表示する
 var todos = ["item 1", "item 2", "item 3"]
 console.log(todos);
 
 console.log('My todos: ', todos)
+
+// 情報を付け加える
+todos.push("item 4")
+
+// 情報を変更する
+todos[0] = "chiyomi"
+
+// 情報を削除する
+todos.splice(3,1)
